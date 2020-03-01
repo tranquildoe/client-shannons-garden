855f007d1de3f4a2436a47ce2f9bcc452e8cb837
Shannon Kimmel <ciervatranquila@gmail.com>
2020-03-01 20:04:24 +0100

    Added default avatar to MyDashboard and re-adjusted the button below

--- src/components/MyDashboard.jsx
+++ src/components/MyDashboard.jsx
@@ -2,18 +2,20 @@ import React, { Component } from 'react'
 import DBImgRectText from "./userDashboard/DBImgRectText"
 import BtnMySeedList from "./userDashboard/BtnMySeedList"
 import BtnMyWishList from "./userDashboard/BtnMyWishList"
+import AvatarImg from "./buildingBlocks/AvatarImg"
 import "./../styles/MyDashboard.css"
 import "./../styles/BtnMySeedList.css"
 import "./../styles/BtnMyWishList.css"
+import "./../styles/AvatarImg.css"
 
 
 export default class MyDashboard extends Component {
     render() {
         return (
             <div className="pageHolder">
-                {/* <div>hiya</div> */}
                 <div className="buttonAndAboutMeHolder">
                     <DBImgRectText></DBImgRectText>
+                    <AvatarImg></AvatarImg>
                     <BtnMySeedList></BtnMySeedList>
                     <BtnMyWishList></BtnMyWishList>
                     <div className="aboutMe">About Me:</div>
---
+++ src/components/buildingBlocks/AvatarImg.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AvatarImg extends Component {
+    render() {
+        return (
+            <div className="avatarHolder">
+                <img className="avatarImage" src="./images/defaultAvatar.png" alt="avatar"/>
+            </div>
+        )
+    }
+}

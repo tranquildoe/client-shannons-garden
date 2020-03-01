54765ece4d966b0ab7bf17216dd7203922770780
Shannon Kimmel <ciervatranquila@gmail.com>
2020-03-01 15:58:35 +0100

    removed commented test line in MyDashboard

--- src/components/MyDashboard.jsx
+++ src/components/MyDashboard.jsx
@@ -4,7 +4,7 @@ export default class MyDashboard extends Component {
     render() {
         return (
             <div>
-                <div>hiya</div>
+                {/* <div>hiya</div> */}
             </div>
         )
     }

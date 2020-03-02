8b9f4318f2e65b0cff091285965a4b5ef1e932dc
Shannon Kimmel <ciervatranquila@gmail.com>
2020-03-02 22:17:28 +0100

    Added Add Seed Button & centered seeds on MySeeds

---
+++ src/components/mySeedList/AddSeedsBtn.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Button extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <button className="stndrdBtn addSeedsBtn">+ Add Seeds</button>
+            </React.Fragment>
+        )
+    }
+}
--- src/components/mySeedList/MySeedList.jsx
+++ src/components/mySeedList/MySeedList.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import ImgMySeedListTextRect from "./ImgMySeedListTextRect"
 import SeedBasic from "./../buildingBlocks/SeedBasic"
+import AddSeedsBtn from "./AddSeedsBtn"
+
+// Stylesheets
 import "./../../styles/SeedBasic.css"
+import "./../../styles/AddSeedsBtn.css"
 
 export default class MySeedList extends Component {
     render() {
         return (
-            <div>
+            <div className="bigHolderMySeedList">
                 <ImgMySeedListTextRect></ImgMySeedListTextRect>
+                <AddSeedsBtn></AddSeedsBtn>
                 <SeedBasic></SeedBasic>
             </div>
         )

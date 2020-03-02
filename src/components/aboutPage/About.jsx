babebc01ddc3dddc2e0a12df4985c7657823fe88
Shannon Kimmel <ciervatranquila@gmail.com>
2020-03-02 18:35:26 +0100

    new image on Mission page

--- src/components/aboutPage/About.jsx
+++ src/components/aboutPage/About.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import ImgAboutTxtRect from "./ImgAboutTxtRect"
+import ImgMissionTall from "./ImgMissionTall"
+
 import "./../../styles/ImgAboutTxtRect.css"
+import "./../../styles/ImgMissionTall.css"
+
 
 export default class About extends Component {
     render() {
         return (
             <div className="bigDivAboutPage">
                 <ImgAboutTxtRect></ImgAboutTxtRect>
+                <ImgMissionTall></ImgMissionTall>
+                <img className="handsImg" src="./images/aboutPage/seedsHands.jpg" alt="seeds cupped in 3 people's hands"/>
             </div>
         )
     }
--- src/components/aboutPage/ImgAboutTxtRect.jsx
+++ src/components/aboutPage/ImgAboutTxtRect.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import ImgMissionTall from "./ImgMissionTall"
 // import Break from "./../buildingBlocks/Break"
-import "./../../styles/ImgMissionTall.css"
 // import "./../../styles/Break.css"
 
 
@@ -12,8 +10,6 @@ export default class ImgRectText extends Component {
                 <img className="imgInBackRect" src="./images/aboutPage/seedsLinesRect.jpg" alt="seeds in lines"/>
                 <div className="titleInFront about">About</div>
                 {/* <Break></Break> */}
-                <ImgMissionTall></ImgMissionTall>
-
             </React.Fragment>
         )
     }

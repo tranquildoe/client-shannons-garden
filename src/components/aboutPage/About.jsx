a7a24e278acc2fd32c68644e254a8674ca56b90c
Shannon Kimmel <ciervatranquila@gmail.com>
2020-03-02 19:38:41 +0100

    Core Values added to About page

--- src/components/aboutPage/About.jsx
+++ src/components/aboutPage/About.jsx
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import ImgAboutTxtRect from "./ImgAboutTxtRect"
 import ImgMissionTall from "./ImgMissionTall"
 import Vision from "./Vision"
+import CoreValues from "./CoreValues"
 
 import "./../../styles/ImgAboutTxtRect.css"
 import "./../../styles/ImgMissionTall.css"
 import "./../../styles/Vision.css"
+import "./../../styles/CoreValues.css"
 
 export default class About extends Component {
     render() {
@@ -15,6 +17,8 @@ export default class About extends Component {
                 <ImgMissionTall></ImgMissionTall>
                 <img className="handsImg" src="./images/aboutPage/seedsHands.jpg" alt="seeds cupped in 3 people's hands"/>
                 <Vision></Vision>
+                <img className="circlehandsImg" src="./images/aboutPage/seedsCircleHands.jpg" alt="seeds in hands in a circle"/>
+                <CoreValues></CoreValues>
             </div>
         )
     }
---
+++ src/components/aboutPage/CoreValues.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Vision extends Component {
+    render() {
+        return (
+            <div>   
+                <div className="titleCoreValues">
+                Core Values:
+                </div>
+                <div className="coreValues">
+                Empowerment<br />
+                Diversity<br />
+                Community <br />
+                Integrity<br />
+                </div>
+            </div>
+        )
+    }
+}

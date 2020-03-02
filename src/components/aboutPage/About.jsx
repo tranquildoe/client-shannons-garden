c4faae34b74c053d7ca722210acbefb7597aa8e2
Shannon Kimmel <ciervatranquila@gmail.com>
2020-03-02 19:01:50 +0100

    Added Vision and adjusted look on About page

--- src/components/aboutPage/About.jsx
+++ src/components/aboutPage/About.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import ImgAboutTxtRect from "./ImgAboutTxtRect"
 import ImgMissionTall from "./ImgMissionTall"
+import Vision from "./Vision"
 
 import "./../../styles/ImgAboutTxtRect.css"
 import "./../../styles/ImgMissionTall.css"
-
+import "./../../styles/Vision.css"
 
 export default class About extends Component {
     render() {
@@ -13,6 +14,7 @@ export default class About extends Component {
                 <ImgAboutTxtRect></ImgAboutTxtRect>
                 <ImgMissionTall></ImgMissionTall>
                 <img className="handsImg" src="./images/aboutPage/seedsHands.jpg" alt="seeds cupped in 3 people's hands"/>
+                <Vision></Vision>
             </div>
         )
     }
---
+++ src/components/aboutPage/Vision.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Vision extends Component {
+    render() {
+        return (
+            <div className="vision">
+                Vision: <br />
+                To have a user-friendly mecca <br />
+                that will help increase biodiversity <br />
+                in seed collections around the world,
+                so we can be better prepared
+                to deal with <br />
+                stressors brought by <br />
+                challenging times.
+            </div>
+        )
+    }
+}

eff1c562dc4f6035151724c3ae4bdf6f53ab0ed5
Shannon Kimmel <ciervatranquila@gmail.com>
2020-03-02 18:19:23 +0100

    About page mission details

--- src/components/aboutPage/ImgMissionTall.jsx
+++ src/components/aboutPage/ImgMissionTall.jsx
@@ -4,8 +4,8 @@ export default class ImgRectText extends Component {
     render() {
         return (
             <React.Fragment>
-                <img className="imgInBackRect stones" src="./images/aboutPage/steppingStonesBack.jpg" alt="stepping stones surrounded by moss"/>
-                <div className="titleInFront">
+                <img className="imgInBackRect stones" src="./images/aboutPage/steppingStonesBackSq.jpg" alt="stepping stones surrounded by moss"/>
+                <div className="titleInFront mission">
                     Mission: <br /> 
                     To provide a user-friendly app <br /> where you can set up <br /> seed trades and keep <br /> your seed lists.
                 </div>

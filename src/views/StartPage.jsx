d7706ebce885ac8e618ef449f06b177a86a7e828
Shannon Kimmel <ciervatranquila@gmail.com>
2020-03-06 01:24:31 +0100

    gorgeous logo on start page!

--- src/views/StartPage.jsx
+++ src/views/StartPage.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import SeedBasic from "./../components/buildingBlocks/SeedBasic";
 import IsForTradeButton from "../components/buildingBlocks/IsForTradeButton";
 
+import "./../styles/StartPage.css"
 
 export default function StartPage() {
   const [isForTrade, setOnlyForTrade] = useState(false);
 
   return (
-    <div>
-      <h1 className="title">Home page</h1>
+    <div className="logoAndButtonHolder">
+      {/* <h1 className="title">Home page</h1> */}
+      <img className="logo" src="./images/new/sgLogo.png" alt="sweet logo"/>
+      
       <IsForTradeButton
         clbk={() => setOnlyForTrade(!isForTrade)}
         status={isForTrade}
